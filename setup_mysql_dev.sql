bccbf04744e8aab5b4dce757e276c763b6164f37
FabPaul <paulfopa0@gmail.com>
2023-08-22 14:45:21 +0100

    Missed flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,4 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hdnd_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
