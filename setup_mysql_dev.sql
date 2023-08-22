96f3651733e1ab870c54105c1ca711323039bbf9
FabPaul <paulfopa0@gmail.com>
2023-08-22 14:43:47 +0100

    Task3, MySQL script setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+--Script that prepares a MySQL server for the given project--
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hdnd_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
